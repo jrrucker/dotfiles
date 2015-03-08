ba1bcba5369b73ca519dca5f5bbbb46e85a831e2
Josh Rucker <Josh.Rucker@inin.com>
2015-03-08 14:49:26 -0400

    Add node package installation scripti

---
+++ node.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure you've installed node first. You can
+# run brew.sh to do this.
+
+# Install Bower Package Manager
+
+sudo npm install -g bower
+
+# Install Automation Tools
+
+sudo npm install -g grunt-cli
+sudo npm install --global gulp
+
+# Install Yeoman for Project Scaffolding
+
+sudo npm install -g yo
+
+# Install Common Scaffolds
+
+sudo npm install -g generator-karma
+sudo npm install -g generator-angular
+sudo npm install -g generator-backbone
+sudo npm install -g generator-wordpress
+sudo npm install -g generator-gruntplugin
